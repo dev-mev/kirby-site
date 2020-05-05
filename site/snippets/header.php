91b600b5b6b38fa6a63fd5b7c9c5e7cbf86396ee
dev-mev <40147757+dev-mev@users.noreply.github.com>
2020-05-05 08:42:25 -0700

    fix indexing logic, update login layout

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="description" content="<?= $page->mdescription()->html() ?>">
 
-    <?php if($page->toggle()->toBool() === true): ?>
+    <?php if($page->toggle()->toBool() === false): ?>
     <meta name="robots" content="noindex">
     <?php endif ?>
 
